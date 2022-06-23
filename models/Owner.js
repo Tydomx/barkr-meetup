4160c49a54372481afc19c570c80440a839e88d0
Tydomx <michaelthai16@gmail.com>
2022-06-23 17:58:37 -0500

    login system added

--- models/Owner.js
+++ models/Owner.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our Owner model
-class Owner extends Model { }
+class Owner extends Model { 
+  // set up method to run on instance data to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 Owner.init(
