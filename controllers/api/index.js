99ee169cbe36d5e6b698d60abb68e5be42972fbb
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-06-26 17:55:00 -0500

    updated imported routes to api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const ownerRoutes = require('./owner-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/owners', ownerRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments',commentRoutes);
 
 module.exports = router;
