850ecb20d6768a6a98f7df427f0e9b7dcca406e6
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-06-28 17:34:04 -0500

    adjusted tests to fix helpers bug

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, Owner, Comment } = require('../models');
+const { Post, Owner, Comment, Vote } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
