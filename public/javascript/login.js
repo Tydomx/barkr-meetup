4be4abb31c5b568371352ef9a19b81702007caf8
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-06-27 20:18:21 -0500

    removed modal; added dog info to sign up form; fixed event handler; redid some javascript

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -39,13 +39,23 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const dogname = document.querySelector('#dogname-signup').value.trim();
+    const dogbreed = document.querySelector('#dogbreed-signup').value.trim();
+    const dogsize = document.querySelector('#dogsize-signup').value.trim();
+    const location = document.querySelector('#location-signup').value.trim();
+    const dogdescription = document.querySelector('#dogdescription-signup').value.trim();
   
     // does username need to be user_name???
-    if (username && email && password) {
+    if (ownername && dogname && dogbreed && dogsize && location && dogdescription && username && email && password) {
       const response = await fetch('/api/owners', {
         method: 'post',
         body: JSON.stringify({
           owner_name:ownername,
+          dog_name:dogname,
+          dog_breed:dogbreed,
+          dog_size:dogsize,
+          location,
+          dog_description:dogdescription,
           user_name:username,
           email,
           password
@@ -55,6 +65,7 @@ async function signupFormHandler(event) {
       // check the response status
     if (response.ok) {
         console.log('success');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
